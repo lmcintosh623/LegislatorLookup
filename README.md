e92f175feb5acfea92ac375bf4d46241542a79b9
lmcintosh623 <lmcintosh623@gmail.com>
2021-02-25 13:38:18 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# LegislatorLookup
-
-The current hosting of the website can be found through any web browser and navigating to:
-https://lmcintosh623.github.io/LegislatorLookup/
-
-Alternatively, cloning or downloading the project on the gh-pages branch, navigating to the
-docs folder and opening index.html will achieve the same effect without an internet connection.
---
+++ precache-manifest.7d59b1967c78ba8caf6db1669f999bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9263dab1b1eebffd5df2b28fdcd56044",
+    "url": "/LegislatorLookup/index.html"
+  },
+  {
+    "revision": "35ac5d27a54c4b407a36",
+    "url": "/LegislatorLookup/static/css/main.99c6d36f.chunk.css"
+  },
+  {
+    "revision": "5cfd9463fdfb163c3ebc",
+    "url": "/LegislatorLookup/static/js/2.5412c5ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/LegislatorLookup/static/js/2.5412c5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ac5d27a54c4b407a36",
+    "url": "/LegislatorLookup/static/js/main.1466d513.chunk.js"
+  },
+  {
+    "revision": "8604dff3603a1432d769",
+    "url": "/LegislatorLookup/static/js/runtime-main.03b81396.js"
+  }
+]);
