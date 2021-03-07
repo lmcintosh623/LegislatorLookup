af54f7f22ec5468c5bc17627d4a8b4e223e24ca7
nemethj2 <nemethj2@wwu.edu>
2021-03-06 20:00:38 -0800

    updated packages

--- src/components/pages/Explore.js
+++ src/components/pages/Explore.js
@@ -1,13 +1,13 @@
-import Promise from 'promise-polyfill';
-import { render } from '@testing-library/react';
+import React from 'react';
+import { Dropdown } from '../LegBodyDropdown.js';
 import {Table} from './Table'; 
 
 
 export default function Explore() {
   return(
     <div className="App">
+      <Dropdown></Dropdown>
       <Table/>
     </div>
   )
-
 }
