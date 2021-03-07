f54ffe3519bf0a8910024a8ba42dc567787754fe
nemethj2 <nemethj2@wwu.edu>
2021-03-07 15:49:52 -0800

    included added row functionality and an href to an external url

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -28,6 +28,12 @@ export const Table=()=>{
             {
               title: 'Email', field: 'email' 
             },
+
+            {
+              filtering: false,
+              title: 'Website', field: 'openstates_url',
+              render: rowData=> (<a href={rowData.openstates_url}>Click Here</a> )
+            }
           ]}
           data={query =>
             new Promise((resolve, reject) => {
@@ -66,5 +72,7 @@ export const Table=()=>{
         />
     </div>)
 }
+
+
 
   
