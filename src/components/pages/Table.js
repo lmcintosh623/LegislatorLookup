03ddc8e7122b47e17e1f7b6e944e5db0da9c2063
gideonwolfe <wolfeg2@wwu.edu>
2021-03-07 13:20:32 -0800

    pagination works

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -15,7 +15,9 @@ export const Table=()=>{
             },
             { 
               title: 'Full Name', 
-              field: 'name' },
+              field: 'name',
+              type: 'string'
+            },
             { 
               title: 'Party', field: 'party',lookup: { 'Democratic': 'Democratic', 'Republican': 'Republican' } 
             },
@@ -28,7 +30,12 @@ export const Table=()=>{
           ]}
           data={query =>
             new Promise((resolve, reject) => {
-              let url = `https://v3.openstates.org/people?jurisdiction=Washington&include=sources&include=other_identifiers&per_page=4&org_classification=lower&apikey=7f7afdc0-15e1-461e-9d2c-1dec521187c8&page=1`
+              // let url = `https://v3.openstates.org/people?jurisdiction=Washington&include=sources&include=other_identifiers&per_page=4&org_classification=lower&apikey=7f7afdc0-15e1-461e-9d2c-1dec521187c8`
+              let url = `https://v3.openstates.org/people?jurisdiction=Washington&include=sources&include=other_identifiers&per_page=4&org_classification=lower&apikey=bf41dac1-543d-4b1d-a373-ebf272baa921`
+              url += '&page=' + (query.page + 1)
+              // TODO add variables
+              // url += `&org_classification=${org_classification}`
+              // url += `&jurisdiction=${jurisdiction}`
 
               fetch(url)
                 .then(response => response.json())
@@ -36,19 +43,21 @@ export const Table=()=>{
                   console.log(result)
                   resolve({
                     data: result.results,
+                    page: result.pagination.page - 1,
+                    totalPages: result.pagination.max_page,
+                    totalCount: result.pagination.total_items,
                   })
                 })
             })
           }options={{
             search: true,
             paging: true,
-            filtering: true,
+            filtering: false,
             exportButton: true,
             headerStyle: {
               fontSize: '20',
               backgroundColor: '#7A56D1',
               color: '#FFFFFF',
-              zIndex: 1,
             },
         }}
         />
