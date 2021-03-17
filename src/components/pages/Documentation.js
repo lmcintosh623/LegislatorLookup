83a4611ddd301378115b08a660c09c0f6f6f98ec
Laura Martin <marti335@wwu.edu>
2021-03-16 23:21:26 -0700

    Update Documentation.js

--- src/components/pages/Documentation.js
+++ src/components/pages/Documentation.js
@@ -1,9 +1,51 @@
 import React, { useState } from 'react';
+import './doc.css';
 
 export default function Documentation() {
 
   return (
-    <p> SOME TEXT</p>
+    <body class='container2'>
+      <p class = 'doc-title'>How to use LegislatorLookup</p>
+      <p class = 'doc-par1'>
+        Voting is important, especially when those elected determine the structure our society functions within
+        </p>
+
+        <p class = 'doc-par2'>
+
+        Our goal is to offer a platform that gives the information you need on politicians to make the most informed vote 
+        </p>
+
+        <p class = 'doc-par3'>
+
+        right now we offer the ability to:
+        </p>
+
+        <p class = 'doc-par4'>
+
+         -Look up legislators in your area via the Legislator tab and navigate to their web page
+         </p>
+
+         <p class = 'doc-par4'>
+
+         -Search through a variety of bills as well as who has cast votes on those bills in the Bills tab
+         </p>
+
+         <p class = 'doc-par6'>
+
+         -Get Basic information on your states current politicians and recent bills in the Main tab
+         </p>
+
+         <p class = 'doc-par7'>
+
+         -As well as an opportunity to navigate to voter registration
+         </p>
+
+       <p class = 'doc-par8'>
+        We get our information from OpenStates API that has unbiased data based on your states facts
+      </p>
+
+    </body>
   );
 
 }
+
